bea14cca07a1e83626619b00b9d2b3100e6171f5
Galitsky Dmytro <melles.android@gmail.com>
2024-02-22 19:33:19 +0200

    Added new setting groups to the configuration file.
    Changes include:
    - Splitting settings parameters into two groups: "General Settings" and "Advanced Settings".
    - Parameters "maxZoomFactor" and "moveViewDistance" placed in the "General Settings" group for better organization.
    - Parameter "reduceUnexpectedMovement" placed in the "Advanced Settings" group for separate control.
    - Added comments for better understanding of the settings structure.

--- Max_Camera_Distance.lua
+++ Max_Camera_Distance.lua
@@ -4,6 +4,7 @@ local AceConfig = LibStub("AceConfig-3.0")
 local AceConfigDialog = LibStub("AceConfigDialog-3.0")
 local AceDB = LibStub("AceDB-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("Max_Camera_Distance")
+local CVar = C_CVar
 
 local db
 local MAX_ZOOM_FACTOR = 2.6
@@ -17,6 +18,7 @@ local defaults = {
 	profile = {
 		maxZoomFactor = MAX_ZOOM_FACTOR,
 		moveViewDistance = MAX_MOVE_DISTANCE,
+		reduceUnexpectedMovement = true, -- Додано значення за замовчуванням
 	},
 }
 
@@ -28,6 +30,8 @@ local function AdjustCamera()
 	if not InCombatLockdown() then
 		SetCVar("cameraDistanceMaxZoomFactor", db.profile.maxZoomFactor)
 		MoveViewOutStart(db.profile.moveViewDistance)
+		-- Встановлює значення cameraReduceUnexpectedMovement
+		CVar.SetCVar("cameraReduceUnexpectedMovement", db.profile.reduceUnexpectedMovement and "1" or "0")
 
 		if f.Ticker then
 			f.Ticker:Cancel()
@@ -36,9 +40,10 @@ local function AdjustCamera()
 	end
 end
 
-local function ChangeCameraSettings(newMaxZoomFactor, newMoveViewDistance, message)
+local function ChangeCameraSettings(newMaxZoomFactor, newMoveViewDistance, newReduceMovementValue, message)
 	db.profile.maxZoomFactor = newMaxZoomFactor
 	db.profile.moveViewDistance = newMoveViewDistance
+	db.profile.reduceUnexpectedMovement = newReduceMovementValue
 	AdjustCamera()
 	SendMessage(message)
 end
@@ -50,9 +55,11 @@ local function OnAddonLoaded(self, event, loadedAddonName)
 		f.Ticker = C_Timer.NewTicker(1, function()
 			local currentMaxZoomFactor = tonumber(GetCVar("cameraDistanceMaxZoomFactor"))
 			local currentMoveViewDistance = tonumber(GetCVar("cameraDistanceMoveSpeed"))
+			local currentReduceMovementValue = tonumber(GetCVar("cameraReduceUnexpectedMovement"))
 
-			if currentMaxZoomFactor ~= db.profile.maxZoomFactor or currentMoveViewDistance ~= db.profile.moveViewDistance then
-				ChangeCameraSettings(db.profile.maxZoomFactor, db.profile.moveViewDistance, L["SETTINGS_CHANGED"])
+			if currentMaxZoomFactor ~= db.profile.maxZoomFactor or currentMoveViewDistance ~= db.profile.moveViewDistance or currentReduceMovementValue ~= db.profile.reduceUnexpectedMovement then
+				ChangeCameraSettings(db.profile.maxZoomFactor, db.profile.moveViewDistance,
+					db.profile.reduceUnexpectedMovement, L["SETTINGS_CHANGED"])
 			end
 		end)
 
@@ -64,31 +71,63 @@ local options = {
 	name = "Max Camera Distance",
 	type = "group",
 	args = {
-		maxZoomFactor = {
-			type = "range",
-			name = L["MAX_ZOOM_FACTOR"],
-			desc = L["MAX_ZOOM_FACTOR_DESC"],
-			min = MIN_ZOOM_FACTOR,
-			max = MAX_ZOOM_FACTOR,
-			step = 0.1,
-			get = function() return db.profile.maxZoomFactor end,
-			set = function(_, value)
-				db.profile.maxZoomFactor = value
-				ChangeCameraSettings(db.profile.maxZoomFactor, db.profile.moveViewDistance, L["SETTINGS_SET_TO_MAX"])
-			end,
+		generalSettings = {
+			type = "group",
+			name = "General Settings",
+			inline = true,
+			order = 1,
+			args = {
+				maxZoomFactor = {
+					type = "range",
+					name = L["MAX_ZOOM_FACTOR"],
+					desc = L["MAX_ZOOM_FACTOR_DESC"],
+					min = MIN_ZOOM_FACTOR,
+					max = MAX_ZOOM_FACTOR,
+					step = 0.1,
+					get = function() return db.profile.maxZoomFactor end,
+					set = function(_, value)
+						db.profile.maxZoomFactor = value
+						ChangeCameraSettings(db.profile.maxZoomFactor, db.profile.moveViewDistance,
+							db.profile.reduceUnexpectedMovement, L["SETTINGS_SET_TO_MAX"])
+					end,
+					order = 1,
+				},
+				moveViewDistance = {
+					type = "range",
+					name = L["MOVE_VIDEO_DISTANCE"],
+					desc = L["MOVE_VIDEO_DISTANCE_DESC"],
+					min = MIN_MOVE_DISTANCE,
+					max = MAX_MOVE_DISTANCE,
+					step = 1000,
+					get = function() return db.profile.moveViewDistance end,
+					set = function(_, value)
+						db.profile.moveViewDistance = value
+						ChangeCameraSettings(db.profile.maxZoomFactor, db.profile.moveViewDistance,
+							db.profile.reduceUnexpectedMovement, L["SETTINGS_SET_TO_MAX"])
+					end,
+					order = 2,
+				},
+			},
 		},
-		moveViewDistance = {
-			type = "range",
-			name = L["MOVE_VIDEO_DISTANCE"],
-			desc = L["MOVE_VIDEO_DISTANCE_DESC"],
-			min = MIN_MOVE_DISTANCE,
-			max = MAX_MOVE_DISTANCE,
-			step = 1000,
-			get = function() return db.profile.moveViewDistance end,
-			set = function(_, value)
-				db.profile.moveViewDistance = value
-				ChangeCameraSettings(db.profile.maxZoomFactor, db.profile.moveViewDistance, L["SETTINGS_SET_TO_MAX"])
-			end,
+		advancedSettings = {
+			type = "group",
+			name = "Advanced Settings",
+			inline = true,
+			order = 2,
+			args = {
+				reduceUnexpectedMovement = {
+					type = "toggle",
+					name = L["REDUCE_UNEXPECTED_MOVEMENT"],
+					desc = L["REDUCE_UNEXPECTED_MOVEMENT_DESC"],
+					get = function() return db.profile.reduceUnexpectedMovement end,
+					set = function(_, value)
+						db.profile.reduceUnexpectedMovement = value
+						ChangeCameraSettings(db.profile.maxZoomFactor, db.profile.moveViewDistance,
+							db.profile.reduceUnexpectedMovement, L["SETTINGS_CHANGED"])
+					end,
+					order = 1,
+				},
+			},
 		},
 	},
 }
@@ -104,13 +143,16 @@ SLASH_DIS_AVG1 = "/dis_avg"
 SLASH_DIS_MIN1 = "/dis_min"
 
 function SlashCmdList.DIS_MAX()
-	ChangeCameraSettings(MAX_ZOOM_FACTOR, MAX_MOVE_DISTANCE, L["SETTINGS_SET_TO_MAX"])
+	ChangeCameraSettings(MAX_ZOOM_FACTOR, MAX_MOVE_DISTANCE, db.profile.reduceUnexpectedMovement,
+		L["SETTINGS_SET_TO_MAX"])
 end
 
 function SlashCmdList.DIS_AVG()
-	ChangeCameraSettings(AVERAGE_ZOOM_FACTOR, AVERAGE_MOVE_DISTANCE, L["SETTINGS_SET_TO_AVERAGE"])
+	ChangeCameraSettings(AVERAGE_ZOOM_FACTOR, AVERAGE_MOVE_DISTANCE, db.profile.reduceUnexpectedMovement,
+		L["SETTINGS_SET_TO_AVERAGE"])
 end
 
 function SlashCmdList.DIS_MIN()
-	ChangeCameraSettings(MIN_ZOOM_FACTOR, MIN_MOVE_DISTANCE, L["SETTINGS_SET_TO_MIN"])
+	ChangeCameraSettings(MIN_ZOOM_FACTOR, MIN_MOVE_DISTANCE, db.profile.reduceUnexpectedMovement,
+		L["SETTINGS_SET_TO_MIN"])
 end
