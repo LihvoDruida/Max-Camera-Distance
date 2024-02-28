3c51ef6c572dd2fc5392bf2436be75873939bab0
Galitsky Dmytro <melles.android@gmail.com>
2024-02-28 21:35:04 +0200

    Update deDE (German) Language by SunnySunflow

--- locale/deDE.lua
+++ locale/deDE.lua
@@ -1,8 +1,21 @@
 local Locals = LibStub("AceLocale-3.0"):NewLocale("Max_Camera_Distance", "deDE")
 if not Locals then return end
 
-Locals["SETTINGS_SET_TO_MAX"] = "Kameraeinstellungen auf maximale Werte gesetzt."
-Locals["SETTINGS_SET_TO_AVERAGE"] = "Kameraeinstellungen auf durchschnittliche Werte gesetzt."
-Locals["SETTINGS_SET_TO_MIN"] = "Kameraeinstellungen auf minimale Werte gesetzt."
-Locals["SETTINGS_SET_TO_DEFAULT"] = "Kameraeinstellungen auf Standardwerte gesetzt."
-Locals["SETTINGS_CHANGED"] = "Kameraeinstellungen wurden geändert."
+L = Locals or {}
+L["MAX_ZOOM_FACTOR"] = "Maximaler Zoomfaktor"
+L["MAX_ZOOM_FACTOR_DESC"] = "Stellt den maximalen Zoomfaktor ein."
+L["MOVE_VIDEO_DISTANCE"] = "Sichtweite verschieben"
+L["MOVE_VIDEO_DISTANCE_DESC"] = "Stellt die Sichtweite zum Verschieben ein."
+--[[Translation missing --]]
+L["REDUCE_UNEXPECTED_MOVEMENT"] = "Reduce Unexpected Camera Movement"
+--[[Translation missing --]]
+L["REDUCE_UNEXPECTED_MOVEMENT_DESC"] = "Enable this option to reduce unexpected camera movements during gameplay."
+--[[Translation missing --]]
+L["RESAMPLE_ALWAYS_SHARPEN"] = "Resample Always Sharpen"
+--[[Translation missing --]]
+L["RESAMPLE_ALWAYS_SHARPEN_DESC"] = "Run sharpness pass, even if not using AMD FSR Upscale [0,1]"
+L["SETTINGS_CHANGED"] = "Die Kameraeinstellungen wurden geändert."
+L["SETTINGS_SET_TO_AVERAGE"] = "Kameraeinstellungen auf Durchschnittswerte setzten."
+L["SETTINGS_SET_TO_DEFAULT"] = "Kameraeinstellungen auf Standardwerte setzten."
+L["SETTINGS_SET_TO_MAX"] = "Kameraeinstellungen auf Maximalwerte setzten."
+L["SETTINGS_SET_TO_MIN"] = "Kameraeinstellungen auf Minimalwerte setzten."
